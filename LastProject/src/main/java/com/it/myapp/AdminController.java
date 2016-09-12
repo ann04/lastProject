2c4e0d952e3a9d2ac2228c146bf881076f2ed52f
it <it@DESKTOP-KKAQ0RK>
2016-09-12 15:25:32 +0900

    ccc

--- LastProject/src/main/java/com/it/myapp/AdminController.java
+++ LastProject/src/main/java/com/it/myapp/AdminController.java
@@ -3,6 +3,8 @@ package com.it.myapp;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.it.model.MovieVO;
 import com.it.service.AdminService;
@@ -13,9 +15,10 @@ public class AdminController {
 	@Inject
 	AdminService service;
 	
+	@RequestMapping(value="movieinsert", method=RequestMethod.POST)
 	public String movieInsert(MovieVO vo) throws Exception{
 		service.movieInsert(vo);
-		return "";
+		return "/amdin/adminmain";
 	}
 	
 }
